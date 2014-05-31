1b1fd65902ba5f279a2a05f810f2d69948b501aa
Fernando Scasserra <fersca@hotmail.com>
2014-05-31 01:26:57 -0300

    cambios

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -69,7 +69,7 @@ angular.module('starter.controllers', ['starter.services','ionic'])
 	var res = $resource(cadena);
  	var Result = res.get({},function(res1) {	 
 		$scope.productos = res1.results;
-		$scope.marca = $stateParams.brand
+		$scope.marca = $stateParams.brand;
 		$ionicLoading.hide();
  	});
 
@@ -81,6 +81,42 @@ angular.module('starter.controllers', ['starter.services','ionic'])
   //}, 700);
 
 })
+
+.controller('MarcasCtrl', function($scope, $resource) {
+	  	
+	var res = $resource('https://api.mercadolibre.com/sites/MLA/apparel/brands');
+ 	var Result = res.query({},function(res1) {	 
+	
+		//Suplanta logo small por comun si no lo hay
+		var index1;
+		for (index1 = 0; index1 < res1.length; index1++) {
+			if (res1[index1].brands[0].small_logo.length==0){
+				res1[index1].brands[0].small_logo=res1[index1].brands[0].logo;
+			}
+		}
+ 		
+ 		//carga los logos en un array de a 3
+ 		var index;
+ 		var listaFinal = [];
+		for (index = 0; index < res1.length; index=index+3) {
+			var lista = [res1[index],res1[index+1],res1[index+2]];
+		    listaFinal.push(lista);
+		}
+
+		//completa la lista con los primeros si no es multiplo de 3
+		if (res1.length%3==2){
+			listaFinal[listaFinal.length-1][2] = listaFinal[0][0]; 
+		} 
+		if (res1.length%3==1){
+			listaFinal[listaFinal.length-1][2] = listaFinal[0][0];
+			listaFinal[listaFinal.length-1][1] = listaFinal[0][1]; 
+		} 
+
+		$scope.playlists = listaFinal;		
+ 	});
+  
+})
+
 
 .controller('ProductCtrl', function($scope, $resource, $stateParams,$ionicLoading, $timeout) {
   // Setup the loader
